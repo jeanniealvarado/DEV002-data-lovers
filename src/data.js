dccc5d3a5c4dd731092e70458ec6531c1a6e8333
Karla Ramirez <karlagramirezj@gmail.com>
2022-11-26 10:53:01 -0300

    se crearon funciones de filtar personaje por genero femenino y masculino, se hicieron botones filtrar por masculino y filtrar por fenino con sus respectias funciones, ya operativos

--- src/data.js
+++ src/data.js
@@ -1,11 +1,59 @@
-// estas funciones son de ejemplo
 
-export const characters = (data) => {
-    
-
+//funcion filtrar por genero masculino
+export const gMasculino = (data) => {
+    let traerPersonajes = data.characters;
+    let nombreYgenero = traerPersonajes.map((element) => ({ name: element.name, gender: element.gender }));
+    let generoMale = nombreYgenero.filter(element => element.gender === "Male");
+    return generoMale;
 };
+//funcion filtar por genero femenino
+export const gFemenino = (data) => {
+    let traerPersonajes = data.characters;
+    let nombreYgenero = traerPersonajes.map((element) => ({ name: element.name, gender: element.gender }));
+    let generoFemale = nombreYgenero.filter(element => element.gender === "Female");
+    return generoFemale;
+}
 
-//export const ejemplo= (data) => {
- // ;
+
+
+
+//let traerPersonajes = data.characters.filter(element => element.name)
+
+//let personajes = [];//
+//console.log(personajes)//
+//traerPersonajes.filter(element => element.name);//
+//let nombrepersonajes = traerPersonajes.map((element) => ({name: element.name}));
+//let casapersonajes = traerPersonajes.map((element) => ({house: element.house}));
+//let nombreYcasaspersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house}));
+//let generopersonajes = traerPersonajes.map((element) => ({gender: element.gender}));
+
+//let nombreCasasGeneropersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house, gender: element.gender}));
+//let casapersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house}));//
+//personajes.push(nombrepersonajes);//
+//console.log(nombrepersonajes);
+//console.log(casapersonajes);
+//console.log(nombreYcasaspersonajes);
+//console.log(generopersonajes);
+//console.log(nombreYgeneropersonajes);
+//console.log(nombreCasasGeneropersonajes);
+
+
+
+//función ordenar libros por año de lanzamiento
+//export const ordenarLibrosAs = (data) => {
+  //  let libros = data.books;
+    //let ascendente = libros.sort(function (a, b){
+      //  return ((a.releaseDay < b.releaseDay) ? -1 : ((a.releaseDay > b.releaseDay) ? 1 : 0));
+//});
+//return ascendente;
+
 //};
 
+//export const ordenarLibrosDesc = (data) => {
+  //  let libros = data.books;
+    //let descendente = libros.sort(function (a, b) {
+      //  return ((a.releaseDay < b.releaseDay) ? 1 : ((a.name > b.name) ? -1 : 0));
+//});
+//return descendente
+    
+//}; 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,71 @@
-//import { characters } from './data.js';//
+import {gMasculino, gFemenino, } from './data.js';
 
 import data from './data/harrypotter/data.js';
 
-console.log(data)
+//boton filtrar genero masculino
+const b1 = document.getElementById ("boton1")
+b1.addEventListener("click",function(){
+    console.log (gMasculino(data));
+ (gMasculino(data));
 
+})
+//boton filtar genero femenino
+const b2 = document.getElementById ("boton2")
+b2.addEventListener("click",function(){
+    console.log (gFemenino(data));
+ (gFemenino(data));
+})
+
+
+
+let tarea = document.getElementById("hombres")
+let tarea2 = document.getElementById("mujeres")
+
+tarea.innerHTML = (gMasculino(data));
+//(gMasculino(data));
 //let personajes = [];//
 //console.log(personajes)//
-let traerPersonajes = data.characters;
-console.log(traerPersonajes);
+
+//let traerPersonajes = data.characters;
+//console.log(traerPersonajes);
 //traerPersonajes.filter(element => element.name);//
-let nombrepersonajes = traerPersonajes.map((element) => ({name: element.name}));
-let casapersonajes = traerPersonajes.map((element) => ({house: element.house}));
-let nombreYcasaspersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house}));
+
+
+//let nombrepersonajes = traerPersonajes.map((element) => ({name: element.name}));
+//let casapersonajes = traerPersonajes.map((element) => ({house: element.house}));
+//let nombreYcasaspersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house}));
+//let generopersonajes = traerPersonajes.map((element) => ({gender: element.gender}));
+//let nombreYgeneropersonajes = traerPersonajes.map((element) => ({name: element.name, gender: element.gender}));
+//let nombreCasasGeneropersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house, gender: element.gender}));
 //let casapersonajes = traerPersonajes.map((element) => ({name: element.name, house: element.house}));//
 //personajes.push(nombrepersonajes);//
-console.log(nombrepersonajes);
-console.log(casapersonajes);
-console.log(nombreYcasaspersonajes)
 
 
+//console.log(nombrepersonajes);
+//console.log(casapersonajes);
+//console.log(nombreYcasaspersonajes);
+//console.log(generopersonajes);
+//console.log(nombreYgeneropersonajes);
+//console.log(nombreCasasGeneropersonajes);
 
 
+//export const charactersmasculino = (data) => {
+    //let traerPersonajes = data.characters;
+    //let nombreYgenero = traerPersonajes.map((element) => ({name: element.name, gender: element.gender}));
+    //let generoMale = nombreYgenero.filter (element => element.gender ==="Male");
+    //let generoFemale = nombreYgenero.filter (element => element.gender ==="Female");
+ 
+    //console.log(nombreYgenero)
+    //console.log(traerPersonajes)
+    //console.log(generoMale)
+    //map ((element) => ({element: element.male}));
 
+//let generofemale = nombreYgenero.map((element) => ({name: element.name, female: element.female}));
+//console.log(generoFemale)
+
+//boton ordenar libros ascendente
+//const b3 = document.getElementById ("boton3")
+//b3.addEventListener("click",function(){
+    //console.log (ordenarLibrosAs(data));
+ //(ordenarLibrosAs(data));
+//})
