6e5d1c936f2ea7aea2610961f1bbc87139c86c91
jeannie alvarado <jeaniie38@gmail.com>
2022-11-30 10:54:50 -0600

    funciones de filtrar y mostrar la data, más darle estilo a los botones y la pagina

--- src/data.js
+++ src/data.js
@@ -1,9 +1,54 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+//import data from "./data/harrypotter/data";
+
+export const filtrarCasas1 = (data) => {
+  let personajes= data.characters;
+  let casas= personajes.map((Element)=>({name:Element.name, house:Element.house}));
+  let casa1= casas.filter(element => element.house === "Gryffindor")
+ return casa1 
+ 
+ }; 
+ export const PromedioG = (data) => {
+  let Gryffi= data.characters.filter(element => element.house === "Gryffindor")
+   Gryffi.reduce((total, currentCharacters, index, array)=>{
+    total += currentCharacters.death
+    return total
+  })
+
+ }
+//  let personajeG= personajes.filter(element => element.name)
+//  let casas= personajeG.map((Element)=>({house:Element.house === "Gryffindor" })); 
+  
+//}; 
+
+
+
+export const filtrarCasas2 = (data) => {
+  let personajes2= data.characters;
+  let casas2= personajes2.map((Element)=>({name:Element.name, house:Element.house}));
+  let casaRave= casas2.filter(element => element.house === "Ravenclaw")
+  return casaRave;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filtrarCasas3 = (data) => {
+  let personajes3= data.characters;
+  let casas3= personajes3.map((Element)=>({name:Element.name, house:Element.house}));
+  let casaSly= casas3.filter(element => element.house === "Slytherin")
+  return casaSly;
 };
+
+export const filtrarCasas4 = (data) => {
+  let personajes4= data.characters;
+  let casas4= personajes4.map((Element)=>({name:Element.name, house:Element.house}));
+  let casaBlack= casas4.filter(element => element.house === "House of Black")
+  return casaBlack;
+};
+
+export const filtrarCasas5 = (data) => {
+  let personajes5= data.characters;
+  let casas5= personajes5.map((Element)=>({name:Element.name, house:Element.house}));
+  let casaHuf= casas5.filter(element => element.house === "Hufflepuff")
+  return casaHuf;
+};
+//export default data;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,98 @@
-import { example } from './data.js';
+import { filtrarCasas1, filtrarCasas2, filtrarCasas3, filtrarCasas4, filtrarCasas5 } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/harrypotter/data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log (data);
+let personajes1= data.characters
+//console.log(personajes)
+let casaG= []
+let casaR= []
+let casaS= []
+let casaH= []
+let casaHu= []
+// console.log(personajes2)
+const mostrar= document.getElementById("Gryffindor")
+mostrar.addEventListener("click", filtrar)
+function filtrar () {
+  casaG= filtrarCasas1(data)
+  casaG.forEach(data => {
+    let mostrarG= document.getElementById("listaG")
+    const lista= document.createElement("li")
+    if (data.house=== "Gryffindor")
+    lista.textContent= data.name
+     mostrarG.appendChild(lista)
+   //document.write("personaje:", data.name)
+  });
+ // console.log(casaG)
+//   for (const Element in data) {
+//     const personajeG= data[Element]
+//     console.log(personajeG)
+//     }
+  }
+
+  
+// let mostrarG= document.querySelector("div")
+// let crear= document.createElement("li")
+// crear.textContent= casaG
+// mostrarG.appendChild(crear)
+   //console.log(casaG)
+
+
+
+
+const mostrar2= document.getElementById("Ravenclaw")
+mostrar2.addEventListener("click", filtrar2)
+function filtrar2(){
+    casaR= filtrarCasas2(data)
+    //console.log(casaR)
+    casaR.forEach(data => {
+    let mostrarR= document.getElementById("listaR")
+    const lista2= document.createElement("li")
+    if (data.house=== "Ravenclaw")
+    lista2.textContent= data.name
+     mostrarR.appendChild(lista2)
+})
+}
+
+const mostrar3= document.getElementById("Slytherin")
+mostrar3.addEventListener("click", filtrar3)
+function filtrar3(){
+    casaS= filtrarCasas3(data)
+    //console.log(casaS)
+    casaS.forEach(data => {
+        let mostrarS= document.getElementById("listaS")
+        const lista3= document.createElement("li")
+        if (data.house=== "Slytherin")
+        lista3.textContent= data.name
+         mostrarS.appendChild(lista3)
+    })
+}
+
+const mostrar4= document.getElementById("House of Black")
+mostrar4.addEventListener("click", filtrar4)
+function filtrar4(){
+    casaH= filtrarCasas4(data)
+    console.log(casaH)
+    casaH.forEach(data => {
+        let mostrarH= document.getElementById("listaH")
+        const lista4= document.createElement("li")
+        if (data.house=== "House of Black")
+        lista4.textContent= data.name
+         mostrarH.appendChild(lista4)
+    })
+}
+
+const mostrar5= document.getElementById("Hufflepuff")
+mostrar5.addEventListener("click", filtrar5)
+function filtrar5 (){
+    casaHu= filtrarCasas5(data)
+    console.log(casaHu)
+    casaHu.forEach(data => {
+        let mostrarHu= document.getElementById("listaHur")
+        const lista5= document.createElement("li")
+        if (data.house=== "Hufflepuff")
+        lista5.textContent= data.name
+         mostrarHu.appendChild(lista5)
+    })
+}
